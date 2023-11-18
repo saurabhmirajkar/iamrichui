9f7f4caafb9ccf7620a6b46ff0ab4805a2ad0161
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-11-18 14:44:48 +0530

    Initial Commit

---
+++ IAmRichUI/IAmRichUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichUIApp.swift
+//  IAmRichUI
+//
+//  Created by Saurabh Mirajkar on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IAmRichUIUITests/IAmRichUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IAmRichUIUITestsLaunchTests.swift
+//  IAmRichUIUITests
+//
+//  Created by Saurabh Mirajkar on 18/11/23.
+//
+
+import XCTest
+
+final class IAmRichUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
